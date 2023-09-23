6d2cbc3b8d0ed93e2fafda7f160c06ee8a4c2dc1
ArsathAhamedSDC <144324849+ArsathAhamedSDC@users.noreply.github.com>
2023-09-23 09:38:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-class BankAccount:
-  def __init__(self, account_number,account_holder_name, initial_balance=0.0):
-    self.__account_number=account_number
-    self.__account_holder_name=account_holder_name 
-    self.__account_balance=initial_balance
-  def deposit(self,amount):
-    if amount>0:
-      self.__account_balance+=amount
-      print("Deposited{}.New balance:{}".format(amount,self.__account_balance))
-    else:
-      print("Invalid deposit amount.please deposit a positive amount.")
-  def withdraw(self,amount):
-    if amount>0 and amount<=self.__account_balance:
-      self.__account_balance-=amount
-      print("Withdraw {}. New balance:{}".format(amount,self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance")
-  def display_balance(self):
-    print("Account balance for {} (Account #{}:{}".format(self.__account_holder_name,self.__account_number,self.__account_balance))
-account=BankAccount(account_number="123456789",account_holder_name="Ragavi", initial_balance=5000.0)
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200)
+"""
+Write a function called linear_search_product that takes the list of products and a target product
+name as input. The function should perform a linear search to find the target product in the list and
+return a list of indices of all occurrences of the product if found, or an empty list if the product is not
+found.
+"""
+
+
+def linearSearchProduct(productList, targetProduct):
+  indices = []
+
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
